105beb9fcfa3d49e029dd6e5aae4c700ab514092
unnathiBR <unnathibr72@gmail.com>
2025-02-17 14:06:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function vote(){
+var name = document.getElementById("#name");
+var age = document.getElementById("#age");
+
+if(age>=18){
+    alert("Eligible to vote");
+}else{
+    alert("Still child")
+
+}
+}
+ 
